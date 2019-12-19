370575e61cc60d24e66aecdcc9bd952978e26f43
sneakyPad <patrickbszwi@gmail.com>
2019-12-19 15:15:45 +0100

    sync

--- Aufgabe 3/XSLT-Aufgabe3.py
+++ Aufgabe 3/XSLT-Aufgabe3.py
@@ -1,30 +1,35 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
 
 
-get_ipython().system('pip install lxml')
-
-
-# In[34]:
-
-
+import argparse
 from lxml import etree
-f_xsl = './program_selector.xsl'
-f_xml = './rytecDE_Basic.sec'
-f_out = './report.xml'
 
 
-transform = etree.XSLT(etree.parse(f_xsl))
+def main(time, channel):
 
-result = transform(etree.parse(f_xml), pTime="'20191126072700 +0100'", pChannel ="'Kika.de'")
-# print(result)
-result.write(f_out)
-
-
-# In[35]:
+    f_xsl = './program_selector_v1.xsl'
+    f_xml = './rytecDE_Basic.sec'
+    f_out = './report.xml'
 
 
-print(result)
-
+    transform = etree.XSLT(etree.parse(f_xsl))
+    time = "\""+ time + "\""
+    channel = "\""+ channel + "\""
+    print('time: ', time)
+    print('channel', channel)
+    
+    result = transform(etree.parse(f_xml), pTime=time, pChannel =channel)
+    #pTime = "'20191126072700 +0100'"
+    #pChannel ="'Kika.de'"
+    print(result)
+    result.write(f_out)
+    
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('-t', '--time',type=str, help='time')
+    parser.add_argument('-c','--channel',
+                    help='channel')
+    
+    args = vars(parser.parse_args())
+    print(args)
+    main(args['time'], args['channel'])
+    
